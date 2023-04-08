1fd37d7de89c6c4d6892dbd824842af83c68a230
Vladimir <tiomnenkiy@gmail.com>
2023-04-08 23:41:17 +0300

    Create Entity.cs

---
+++ Program/3Deditor/3Deditor/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Deditor
+{
+    public class Entity
+    {
+        public float x;
+        public float y;
+        public float z;
+        public string color;
+        public string shape;
+    }
+}
